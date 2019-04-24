11ce7e58d30c23285f3d46ee4d5ef3d9718124bd
kallbuloso <kallbuloso@gmail.com>
2019-04-23 23:03:37 -0300

    resources/views/layouts: head

---
+++ resources/views/layouts/partials/_head.blade.php
@@ -0,0 +1,38 @@
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+        <title>"{{config('app.name')}} | @yield('title_header', config('app.name'))"</title>
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <meta name="description" content="@yield('description', 'Description')">
+        <meta name="author" content="@yield('author', 'Amaral karl')">
+        <meta name="robots" content="noindex, nofollow">
+
+        <!-- Open Graph Meta -->
+        <meta property="og:title" content="@yield('title_header', 'Title')">
+        <meta property="og:site_name" content="{{ config('app.name') }}">
+        <meta property="og:description" content="@yield('description', 'Description')">
+        <meta property="og:type" content="@yield('og-type', 'eletrônica, tecnologia, software, computação, áudio, vídeo')">
+        <meta property="og:url" content="@yield('og-url', Request::url())">
+        <meta property="og:image" content="@yield('og-image', Request::url())">
+
+        <!-- Icons -->
+        <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+        <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+
+        <!-- Fonts and Styles -->
+        @yield('css_before')
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,400i,600,700">
+        <link rel="stylesheet" id="css-main" href="{{ mix('/css/codebase.css') }}">
+
+        <!-- You can include a specific file from public/css/themes/ folder to alter the default color theme of the template. eg: -->
+        <!-- <link rel="stylesheet" id="css-theme" href="{{ mix('/css/themes/corporate.css') }}"> -->
+        @yield('css_after')
+
+        <!-- Scripts -->
+        <script>window.Laravel = {!! json_encode(['csrfToken' => csrf_token(),]) !!};</script>
+    </head>
