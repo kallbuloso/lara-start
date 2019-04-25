6492a4f3a781e8cf29bde05de9089d177220ace4
kallbuloso <kallbuloso@gmail.com>
2019-04-25 03:19:51 -0300

    config/modules:

--- config/modules.php
+++ config/modules.php
@@ -28,7 +28,7 @@ return [
             'routes/web' => 'Routes/web.php',
             'routes/api' => 'Routes/api.php',
             'views/index' => 'Resources/views/index.blade.php',
-            'views/master' => 'Resources/views/layouts/app.blade.php',
+            // 'views/master' => 'Resources/views/layouts/app.blade.php',
             'scaffold/config' => 'Config/config.php',
             'composer' => 'composer.json',
             'assets/js/app' => 'Resources/assets/js/app.js',
